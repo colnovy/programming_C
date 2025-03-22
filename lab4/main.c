7e72cc58dfb92cf4e5f9e44903fb86aaef3ee00e
colnovy <yul.rudikova@gmail.com>
2025-03-22 11:32:57 +1000

    Add files via upload

--- lab4/main.c
+++ lab4/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "perplosh.h"
+#include "triangle.h"
 /* 
 ЗАДАНИЕ 4
 	Написать функции, вычисляющие периметр и площадь треугольника по значе-ниям длин трёх его сторон. Разместить их в отдельном модуле (отличном от того, где располагается функция main( )).
@@ -12,11 +12,11 @@ int main(){
 	double x, y, z;
 	printf("enter the values of the sides of the triangle ");
 	scanf("%lf%lf%lf",&x,&y,&z);
-	if ((x+y<z) | (x+z<y) | (z+y<x)){
-		printf("error:( this triangle is not real");
+	if (real(x,y,z)){
+		printf("%s%lf%s%s%lf", "perimeter is ", per(x, y, z), "\n", "ploshad' is ", plosh(x, y, z));
 	}
 	else{
-		printf("%s%lf%s%s%lf", "perimeter is ", per(x, y, z), "\n", "ploshad' is ", plosh(x, y, z));
+		printf("error:( this triangle is not real");
 	}
 	return 0;
 }
---
+++ lab4/triangle.h
@@ -0,0 +1,4 @@
+double per(double a, double b, double c);
+double plosh(double a, double b, double c);
+bool real(double a, double b, double c);
+
