0df6c2168708bf3f8cbe1b5003d72ff60e62f23c
colnovy <yul.rudikova@gmail.com>
2025-04-28 21:12:30 +1000

    Add files via upload

---
+++ lab10/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ЗАДАНИЕ 10
+	Написать программу вычисления максимального числа из введенных пользователем в виде одной строки.  
+	Использовать для этого макрос (без функции), на вход которому подается массив чисел и их количество.
+ */
+
+
+#define MAX(array, count) ({int max_number; int i; max_number = (array)[0]; for (i = 1; i < (count); i++) {if (max_number < (array)[i]) {max_number = (array)[i]; } } max_number;})
+
+int main(void)
+{   
+    int array[10];
+    int count=5;
+    printf("enter 5 numbers");
+    int i;
+    for (i=0; i<count; i++){
+    	scanf("%d", &array[i]);
+	}
+
+    printf("max_number: %d\n", MAX(count, array));
+
+    return 0;
+}
